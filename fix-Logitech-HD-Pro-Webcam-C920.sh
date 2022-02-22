0c1f11570103ad77248ecff373c351e1756f54a1
Emmanuel Rodriguez <emmanuel.rodriguez@gmail.com>
2022-02-22 14:05:10 +0100

    Improve the camera detection
    Turns out that on a brand new computer that runs the same OS and wevcam:
    
      Ubuntu 20.04
      HD Pro Webcam C920
    
    The webcam is now named:
    
      Video Capture 4
    
    The script is now using the following strategies:
    
      Name:   HD Pro Webcam C920
      Name:   Video Capture
      Device: /dev/video0

--- fix-Logitech-HD-Pro-Webcam-C920.sh
+++ fix-Logitech-HD-Pro-Webcam-C920.sh
@@ -2,10 +2,19 @@
 
 set -eou pipefail
 
+function get_video_device() {
+    if [[ $# == 0 ]]; then
+        echo "${FUNCNAME[0]}: device_name"
+        exit 1
+    fi
+    local webcam_name="$1"
+    v4l2-ctl --list-devices | grep -A1 "$webcam_name" | tail -n1 | sed -E 's/^\s+//'
+}
+
 webcam_name='HD Pro Webcam C920'
 
 # Get the video device of the logitech camera
-video_device=$(v4l2-ctl --list-devices | grep -A1 "$webcam_name" | tail -n1 | sed -E 's/^\s+//')
+video_device=$(get_video_device "$webcam_name" || get_video_device "Video Capture" || echo /dev/video0)
 
 if [[ -z "$video_device" ]]; then
     echo "Failed to find video device for $webcam_name"
